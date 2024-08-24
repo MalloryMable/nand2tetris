373202a024ea31bd5f481a9a97887d02be3416b3
Mallory Mable <mallorymable@proton.me>
2024-08-23 23:08:35 -0400

    made parser a linked list rather than an arraylist

--- projects/06/HackAssembler/src/Parser.java
+++ projects/06/HackAssembler/src/Parser.java
@@ -1,35 +1,47 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Parser {
-    private final ArrayList<String> tempFile = new ArrayList<>();
-    private int position;
-    private String currentLine;
-    private instructionType type;
+    private Instruction current;
+    private final Instruction head;
 
     public Parser(String fileName) throws FileNotFoundException{
         File file =  new File(fileName);
-        Scanner scanner = new Scanner(file);
-        position = 0;
-        while(scanner.hasNextLine()){
-            currentLine = scanner.nextLine().toUpperCase();
-            currentLine = currentLine.replaceAll("[\\s\\t]+", ""); //trims tabs and spaces
-            //NOTE: because spaces are removed labels ambiguated only by spaces will collide
-
-            if(!currentLine.isEmpty() && !currentLine.startsWith("//")){ //removes blank lines and comments
-
-                if(currentLine.contains("//")){
-                    currentLine = currentLine.substring(0, currentLine.indexOf('/'));
+        try (Scanner scanner = new Scanner(file)) {
+            head = new Instruction("", null);
+            current = head;
+           
+            while(scanner.hasNextLine()){
+                String currentLine;
+                instructionType type;
+                
+                currentLine = scanner.nextLine().toUpperCase();
+                currentLine = currentLine.replaceAll("[\\s\\t]+", ""); //trims tabs and spaces
+                //NOTE: because spaces are removed labels ambiguated only by spaces will collide
+                
+                if(!currentLine.isEmpty() && !currentLine.startsWith("//")){ //removes blank lines and comments
+                    if(currentLine.contains("//")){
+                        currentLine = currentLine.substring(0, currentLine.indexOf('/'));
+                    }
+                    
+                    if(currentLine.charAt(0) == '@') {
+                        type = instructionType.A_COMMAND;
+                    } else if(currentLine.charAt(0) == '(' && currentLine.charAt(currentLine.length()-1) == ')') {
+                        type = instructionType.L_COMMAND;
+                    } else {
+                        //NOTE: This leaves room for errors to make their way into C instructions
+                        type = instructionType.C_COMMAND;
+                    }
+                   
+                    current.next = new Instruction( currentLine, type );
+                    current = current.next;
                 }
-                tempFile.add(currentLine);
             }
+
+            current = head.next;
         }
-        scanner.close();
-        //reset position so we will advance to 0 in our temp file;
-        position = -1;
     }
 
     //made public so the assembler may access them. A little prettier than string matching
@@ -40,65 +52,68 @@ public class Parser {
 
     //checks if the next line is outside of file size
     public boolean hasMoreLines() {
-        if(position + 1 < tempFile.size()){
+        if(current.next != null){
             return true;
         }
-        //if there are no more lines we reset our position marker
-        position = -1;
+        //if there are no more lines we reset our iterator
+        current = head.next;
         return false;
     }
 
+    // this is unsafe as we use the main loop to do this check and want to avoid redundancy
     public void advance () {
-        currentLine = tempFile.get(++position);
-
-        //we identify type each time we look at a new line and make that a property of the parser
-        if(currentLine.charAt(0) == '@') {
-            type = instructionType.A_COMMAND;
-        } else if(currentLine.charAt(0) == '(' && currentLine.charAt(currentLine.length()-1) == ')') {
-            type = instructionType.L_COMMAND;
-        } else {
-            //NOTE: This leaves room for errors to make their way into C instructions
-            type = instructionType.C_COMMAND;
-        }
+        current = current.next;
     }
 
     public instructionType instructionType(){
-        return type;
+        return current.type;
     }
 
     public String symbol() {
-        return (type == instructionType.L_COMMAND) ? currentLine.substring(1, currentLine.length() - 1) : currentLine.substring(1);
+        return (current.type == instructionType.L_COMMAND) ? current.line.substring(1, current.line.length() - 1) : current.line.substring(1);
     }
 
     //Returns destination string or null
     public String dest() {
-        int location = currentLine.indexOf('=');
+        int location = current.line.indexOf('=');
 
         if(location == -1){
             return null;
         }
-        return currentLine.substring(0, location);
+        return current.line.substring(0, location);
     }
 
     //Returns our computation code
     public String comp() {
-        int beginLocation = currentLine.indexOf('=');
+        int beginLocation = current.line.indexOf('=');
         beginLocation = (beginLocation == -1) ? 0 : beginLocation + 1;//advance past '='
 
-        int endLocation = currentLine.indexOf(';');
-        endLocation = (endLocation == -1) ? currentLine.length() : endLocation;
+        int endLocation = current.line.indexOf(';');
+        endLocation = (endLocation == -1) ? current.line.length() : endLocation;
 
-        return currentLine.substring(beginLocation, endLocation);
+        return current.line.substring(beginLocation, endLocation);
     }
 
     //returns a 3 letter jump code or null
     public String jump() {
-        int location = currentLine.indexOf(';');
+        int location = current.line.indexOf(';');
 
         if(location == -1) {
             return null;
         }
-        return currentLine.substring(location+1);
+        return current.line.substring(location + 1);
 
+    }
+
+    private static class Instruction {
+        String line;
+        instructionType type;
+        Instruction next;
+        
+        private Instruction(String line , instructionType type){
+            this.line = line;
+            this.type = type;
+            next = null;
+        }
     }
 }
