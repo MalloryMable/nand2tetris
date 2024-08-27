f4b4a6876b04de066966c8033b30fe0b393552a7
Mallory Mable <mallorymable@proton.me>
2024-08-27 17:51:20 -0400

    Update Parser.java
    Removed the need to hold entire files in memory

--- projects/08/VMTranslator/src/Parser.java
+++ projects/08/VMTranslator/src/Parser.java
@@ -1,56 +1,65 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Parser{
     private String[] lineArray = new String[3];
-    private final ArrayList<String> tempFile = new ArrayList<>();
-    private int position = -1; //initialized so that initial advance() moves to position: 0
     private Main.commandType type;
+    boolean checked = false;
+    boolean moreLines;
+    Scanner scanner;
+    String nextLine;
 
     //Opens file and reads in all valid lines of code to Temp File
     public Parser(String fileName) throws FileNotFoundException {
 
         File file = new File(fileName);
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()) {
-            String currentLine = scanner.nextLine()
+        scanner = new Scanner(file);
+        
+    }
+
+    //checks if the next line is outside of file size
+    public boolean hasMoreLines() {
+        if (!checked) {
+            checked = true;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine()
                     .replaceAll("\\t+", "") //removes all tabs
                     .replaceAll("\\s+", " ") //trims sets of spaces to one space
                     .replaceAll("^\\s", "") //removes leading space
                     .replaceAll("\\s$", ""); //removes trailing space
 
-            if (!(currentLine.isEmpty() || currentLine.startsWith("//"))) { //removes blank lines and comments
+                if (!(line.isEmpty() || line.startsWith("//"))) { //removes blank lines and comments
+                    if (line.contains("//")) {
+                        line = line.substring(0, line.indexOf('/'));
+                    }
 
-                if (currentLine.contains("//")) {
-                    currentLine = currentLine.substring(0, currentLine.indexOf('/'));
+                    nextLine = line;
+                    moreLines = true;
+                    break;
                 }
-                tempFile.add(currentLine);
             }
-        }
-    }
-
-    //checks if the next line is outside of file size
-    public boolean hasMoreLines() {
-        return position + 1 < tempFile.size();
+            moreLines = false;
+        } 
+        return moreLines;
     }
 
     //Moves through Temp File(a list of strings)
     public void advance () {
         //we identify the first word of our instruction cutting off at the first space
-        lineArray = tempFile.get(++position).split("\\s");
+        lineArray = nextLine.split("\\s");
         type = getType(lineArray[0]);
+        checked = false;
 
     }
 
     //returns command type determined at advancement
-    public Main.commandType commandType(){ return type;}
+    public Main.commandType commandType() { return type;}
 
     //returns the first argument as a string
     public String arg1() {
-        return (type == Main.commandType.C_ARITHMETIC)? ineArray[0]: lineArray[1];
+        return (type == Main.commandType.C_ARITHMETIC)? lineArray[0]: lineArray[1];
     }
 
     //returns the second argument as int
